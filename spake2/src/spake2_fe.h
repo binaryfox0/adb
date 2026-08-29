805b5f6ccab9a62bf22f7ae24fbae5b0bad1b2d4
binaryfox0 <phamducduy6122011@gmail.com>
2026-08-30 04:26:34 +0700

    revert to use orlp's ed25519

--- spake2/src/spake2_fe.h
+++ spake2/src/spake2_fe.h
@@ -1,12 +1,11 @@
-#ifndef SPAKE2_FE_H
-#define SPAKE2_FE_H
+#ifndef FE_H
+#define FE_H
 
-#include <stdint.h>
+#include "fixedint.h"
 
-#define SPAKE2__FE_LIMB_COUNT 10
 
 /*
-    spake2__fe means field element.
+    fe means field element.
     Here the field is \Z/(2^255-19).
     An element t, entries t[0]...t[9], represents the integer
     t[0]+2^26 t[1]+2^51 t[2]+2^77 t[3]+2^102 t[4]+...+2^230 t[9].
@@ -14,29 +13,29 @@
 */
 
 
-typedef int32_t spake2__fe_t[SPAKE2__FE_LIMB_COUNT];
+typedef int32_t fe[10];
 
 
-void spake2__fe_0(spake2__fe_t h);
-void spake2__fe_1(spake2__fe_t h);
+void fe_0(fe h);
+void fe_1(fe h);
 
-void spake2__fe_frombytes(spake2__fe_t h, const unsigned char *s);
-void spake2__fe_tobytes(unsigned char *s, const spake2__fe_t h);
+void fe_frombytes(fe h, const unsigned char *s);
+void fe_tobytes(unsigned char *s, const fe h);
 
-void spake2__fe_copy(spake2__fe_t h, const spake2__fe_t f);
-int spake2__fe_isnegative(const spake2__fe_t f);
-int spake2__fe_isnonzero(const spake2__fe_t f);
-void spake2__fe_cmov(spake2__fe_t f, const spake2__fe_t g, unsigned int b);
-void spake2__fe_cswap(spake2__fe_t f, spake2__fe_t g, unsigned int b);
+void fe_copy(fe h, const fe f);
+int fe_isnegative(const fe f);
+int fe_isnonzero(const fe f);
+void fe_cmov(fe f, const fe g, unsigned int b);
+void fe_cswap(fe f, fe g, unsigned int b);
 
-void spake2__fe_neg(spake2__fe_t h, const spake2__fe_t f);
-void spake2__fe_add(spake2__fe_t h, const spake2__fe_t f, const spake2__fe_t g);
-void spake2__fe_invert(spake2__fe_t out, const spake2__fe_t z);
-void spake2__fe_sq(spake2__fe_t h, const spake2__fe_t f);
-void spake2__fe_sq2(spake2__fe_t h, const spake2__fe_t f);
-void spake2__fe_mul(spake2__fe_t h, const spake2__fe_t f, const spake2__fe_t g);
-void spake2__fe_mul121666(spake2__fe_t h, spake2__fe_t f);
-void spake2__fe_pow22523(spake2__fe_t out, const spake2__fe_t z);
-void spake2__fe_sub(spake2__fe_t h, const spake2__fe_t f, const spake2__fe_t g);
+void fe_neg(fe h, const fe f);
+void fe_add(fe h, const fe f, const fe g);
+void fe_invert(fe out, const fe z);
+void fe_sq(fe h, const fe f);
+void fe_sq2(fe h, const fe f);
+void fe_mul(fe h, const fe f, const fe g);
+void fe_mul121666(fe h, fe f);
+void fe_pow22523(fe out, const fe z);
+void fe_sub(fe h, const fe f, const fe g);
 
 #endif
